9184937a20b92d7ee4359441d717c18e65050038
ZEIAD-LAPTOP\zeiad <zazaoskar928@gmail.com>
2025-12-27 16:22:37 +0200

    implement error handling middleware

---
+++ src/main/java/com/mycompany/tictactoeserver/domain/security/ExceptionHandlerMiddleware.java
@@ -0,0 +1,24 @@
+package com.mycompany.tictactoeserver.domain.security;
+
+public class ExceptionHandlerMiddleware {
+
+    // Singleton
+    private static ExceptionHandlerMiddleware instance;
+
+    private ExceptionHandlerMiddleware() {
+    }
+
+    public static ExceptionHandlerMiddleware getInstance() {
+        if (instance == null)
+            instance = new ExceptionHandlerMiddleware();
+        return instance;
+    }
+
+
+    public void handleException(Exception ex) {
+        switch (ex.getMessage()) {
+            // case "error-name":
+        }
+    }
+
+}
