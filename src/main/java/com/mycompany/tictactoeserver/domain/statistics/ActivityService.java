a916e8bf5b09b46a070005903d7810f47d2f268c
Tasneem AbdElhakeem <tasniim.hakiim@gmail.com>
2025-12-30 13:21:43 +0200

    feat: add ActivityService

---
+++ src/main/java/com/mycompany/tictactoeserver/domain/statistics/ActivityService.java
@@ -0,0 +1,56 @@
+package com.mycompany.tictactoeserver.domain.statistics;
+
+import com.mycompany.tictactoeserver.datasource.database.dao.ActivityDao;
+import com.mycompany.tictactoeserver.datasource.model.ActivityPoint;
+import com.mycompany.tictactoeserver.domain.exception.*;
+
+public class ActivityService {
+    private final ActivityDao activityDao;
+    private final ExceptionHandlerMiddleware exceptionHandler;
+
+    public ActivityService() {
+        this.activityDao = new ActivityDao();
+        this.exceptionHandler = ExceptionHandlerMiddleware.getInstance();
+    }
+
+    public boolean startPlayerActivity(String playerId) {
+        try {
+            ActivityPoint activity = new ActivityPoint(playerId);
+            return activityDao.startActivity(activity);
+
+        } catch (ActiveSessionExistsException e) {
+            String[] data = {playerId, e.getMessage()};
+            exceptionHandler.handleException(e, data);
+            return false;
+        }
+    }
+
+    public boolean endPlayerActivity(String playerId) {
+        try {
+            return activityDao.endActivityByPlayerId(playerId);
+
+        } catch (ActivityNotFoundException e) {
+            String[] data = {playerId};
+            exceptionHandler.handleException(e, data);
+            return false;
+
+        } catch (DataAccessException e) {
+            exceptionHandler.handleException(e);
+            return false;
+        }
+    }
+
+    public boolean isPlayerActive(String playerId) {
+        return activityDao.hasActiveSession(playerId);
+    }
+
+    public long getPlayerPlayTime(String playerId) {
+        try {
+            return activityDao.getTotalPlayTimeMinutes(playerId);
+
+        } catch (DataAccessException e) {
+            exceptionHandler.handleException(e);
+            return 0;
+        }
+    }
+}
