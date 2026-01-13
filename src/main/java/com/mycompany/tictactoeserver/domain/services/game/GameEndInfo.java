c104ae384d2725b7fca2de950e5c5643833b56f2
Sherif Ashraf <sherif.ashraf.farooq@gmail.com>
2026-01-13 21:05:07 +0200

    Add Game End Info Data Transfer Object

---
+++ src/main/java/com/mycompany/tictactoeserver/domain/services/game/GameEndInfo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tictactoeserver.domain.services.game;
+
+/**
+ *
+ * @author sheri
+ */
+public class GameEndInfo {
+    
+}
